58425ffa35dfd163b030245fbf4b90d3ccb30229
Jose Urdaneta <urdans@gmail.com>
2018-12-23 15:09:05 -0500

    Finished part 1

---
+++ src/main/java/org/launchcode/models/Category.java
@@ -0,0 +1,38 @@
+package org.launchcode.models;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Entity
+public class Category {
+
+    @Id
+    @GeneratedValue
+    private int id;
+
+    @NotNull
+    @Size(min=1, max=15, message = "Category must not be empty")
+    private String name;
+
+    public Category() {
+    }
+
+    public Category(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+}
